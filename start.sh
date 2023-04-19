8fe3a56162bdd138a88c482130f18423a8bd5726
hase744 <jugglego@gmail.com>
2023-04-19 21:03:34 +0900

    docker-compose本番環境8

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if ["${RAILS_ENV}" = "production"]; then
-    bundle exec rails assets:precompile
-    bundle exec unicorn -p 3000 -c /app/config/unicorn.rbelse
+    #bundle exec rails assets:precompile
+    #bundle exec unicorn -p 3000 -c /app/config/unicorn.rb
 else
-    bundle exec rails s -p 3000 -b '0.0.0.0'"
+    #bundle exec rails s -p 3000 -b '0.0.0.0'
 fi
+bundle exec rails s -p 3000 -b '0.0.0.0'
